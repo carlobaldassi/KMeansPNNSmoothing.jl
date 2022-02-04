90d373631da3bd9483f4d5809f4be16f6a8e13e5
Carlo Baldassi <carlobaldassi@gmail.com>
2022-02-05 00:03:05 +0100

    Sammpling without replacement in init_unif

--- src/KMeansNNPP.jl
+++ src/KMeansNNPP.jl
@@ -195,9 +195,14 @@ function init_centroid_unif(data::Matrix{Float64}, k::Int; kw...)
     m, n = size(data)
     DataLogging.@log "INPUTS m: $m n: $n k: $k"
     t = @elapsed config = begin
+        # NOTE: the sampling uses Fisher-Yates when n < 24k, which is O(n+k);
+        #       or self-avoid-sample (keep a set, resample if collisions happen)
+        #       otherwise, which is O(k)
+        centroid_inds = sample(1:n, k; replace = false)
         centroids = zeros(m, k)
         for j = 1:k
-            i = rand(1:n)
+            # i = rand(1:n)
+            i = centroid_inds[j]
             centroids[:,j] .= data[:,i]
         end
         Configuration(data, centroids)
