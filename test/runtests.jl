89f939b83ba72825da29829e098930a93d5f4198
Carlo Baldassi <carlobaldassi@gmail.com>
2022-02-09 01:59:53 +0100

    Fix tests: actually test refine

--- test/runtests.jl
+++ test/runtests.jl
@@ -170,7 +170,7 @@ end
 
 
 @testset "kmeans REFINE(UNIF)" begin
-    result = kmeans(a3, k, init="smoothnn", init0="unif", verbose=false)
+    result = kmeans(a3, k, init="refine", init0="unif", verbose=false)
     @test length(result.labels) == m
     @test all(∈(1:k), result.labels)
     @test size(result.centroids) == (2,k)
@@ -179,7 +179,7 @@ end
 end
 
 @testset "kmeans REFINE(++)" begin
-    result = kmeans(a3, k, init="smoothnn", init0="++", ncandidates=1, verbose=false)
+    result = kmeans(a3, k, init="refine", init0="++", ncandidates=1, verbose=false)
     @test length(result.labels) == m
     @test all(∈(1:k), result.labels)
     @test size(result.centroids) == (2,k)
@@ -188,7 +188,7 @@ end
 end
 
 @testset "kmeans REFINE(MAXMIN)" begin
-    result = kmeans(a3, k, init="smoothnn", init0="maxmin", verbose=false)
+    result = kmeans(a3, k, init="refine", init0="maxmin", verbose=false)
     @test length(result.labels) == m
     @test all(∈(1:k), result.labels)
     @test size(result.centroids) == (2,k)
@@ -197,7 +197,7 @@ end
 end
 
 @testset "kmeans REFINE(SCALA)" begin
-    result = kmeans(a3, k, init="smoothnn", init0="scala", verbose=false)
+    result = kmeans(a3, k, init="refine", init0="scala", verbose=false)
     @test length(result.labels) == m
     @test all(∈(1:k), result.labels)
     @test size(result.centroids) == (2,k)
